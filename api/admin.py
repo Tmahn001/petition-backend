9f58f2e5c60c285b1f89a873f4e91f65fb7ef101
Tmahn001 <toluiwalewa@gmail.com>
2023-08-15 11:14:36 +0100

    uuid-admin-fix

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Petition, InvolvedParty, Signature
 
-# Register your models here.
+# Define ModelAdmin classes for your models
+class PetitionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creator', 'created_at', 'required_signatures_range')
+    list_filter = ('created_at',)
+    search_fields = ('title', 'creator__username')
+    filter_horizontal = ('signatures', 'involved_parties')
+
+class InvolvedPartyAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'phone_number')
+    search_fields = ('full_name', 'email')
+
+class SignatureAdmin(admin.ModelAdmin):
+    list_display = ('signer_name', 'student_email', 'petition', 'signed_at')
+    list_filter = ('signed_at',)
+    search_fields = ('signer_name', 'student_email', 'petition__title')
+
+# Register your models using the custom ModelAdmin classes
+admin.site.register(Petition, PetitionAdmin)
+admin.site.register(InvolvedParty, InvolvedPartyAdmin)
+admin.site.register(Signature, SignatureAdmin)
+
