d2453064fd0c0251732a52e1ce7f352fce958bc7
Tmahn001 <toluiwalewa@gmail.com>
2023-08-16 10:52:57 +0100

    user-details

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PetitionViewSet, SignatureViewSet
+from .views import PetitionViewSet, SignatureViewSet, UserDetailsView
 
 # Create a router and register viewsets
 router = DefaultRouter()
@@ -9,6 +9,7 @@ router.register(r'petitions', PetitionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('me/', UserDetailsView.as_view(), name='me'),
     path('petitions/<uuid:petition_id>/sign/', SignatureViewSet.as_view({'post': 'sign'}), name='sign-petition'),
 ]
 
